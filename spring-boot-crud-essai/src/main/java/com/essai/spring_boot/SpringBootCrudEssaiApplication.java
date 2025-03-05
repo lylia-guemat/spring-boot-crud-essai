e5e2c6475db3c8baa09a75abd52b1362e53240a6
lguemat <lguemat@karavel.com>
2025-03-05 22:22:07 +0100

    Initial commit

---
+++ spring-boot-crud-essai/src/main/java/com/essai/spring_boot/SpringBootCrudEssaiApplication.java
@@ -0,0 +1,13 @@
+package com.essai.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudEssaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudEssaiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-crud-essai/src/test/java/com/essai/spring_boot/SpringBootCrudEssaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essai.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudEssaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
